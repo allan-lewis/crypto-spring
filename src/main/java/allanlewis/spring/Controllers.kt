0e38ab16ef5f328a2d4eabb9442194470f185e49
allan-lewis <allan.e.lewis@gmail.com>
2022-02-15 13:12:51 -0500

    Add a controller that can retrieve an order

--- src/main/java/allanlewis/spring/Controllers.kt
+++ src/main/java/allanlewis/spring/Controllers.kt
@@ -1,6 +1,8 @@
 package allanlewis.spring
 
 import allanlewis.api.Product
+import allanlewis.api.ReadOrder
+import allanlewis.api.RestApi
 import allanlewis.positions.PositionManager
 import allanlewis.positions.PositionSummary
 import allanlewis.products.ProductRepository
@@ -11,7 +13,7 @@ import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/products")
-class ProductController(@Autowired private val productRepository: ProductRepository) {
+class ProductsController(@Autowired private val productRepository: ProductRepository) {
 
     @GetMapping
     fun get(): Publisher<Product> {
@@ -27,7 +29,7 @@ class ProductController(@Autowired private val productRepository: ProductReposit
 
 @RestController
 @RequestMapping(path = ["/positions"])
-class PositionController(@Autowired private val positionManager: PositionManager) {
+class PositionsController(@Autowired private val positionManager: PositionManager) {
 
     @PostMapping(path = ["/{productId}"])
     fun post(@PathVariable productId: String): Publisher<String> {
@@ -42,6 +44,17 @@ class PositionController(@Autowired private val positionManager: PositionManager
     @GetMapping(path = ["/{positionId}"])
     fun get(@PathVariable positionId: String): Publisher<PositionSummary> {
         return positionManager.getPosition(positionId)
+    }
+
+}
+
+@RestController
+@RequestMapping(path = ["/orders"])
+class OrdersController(@Autowired private val restApi: RestApi) {
+
+    @GetMapping(path = ["/{id}"])
+    fun get(@PathVariable id: String): Publisher<ReadOrder> {
+        return restApi.getOrder(id)
     }
 
 }
